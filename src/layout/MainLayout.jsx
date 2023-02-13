466b399753ebce65c45ff6930e29582f6480224b
carlos brito <cjbritoc@gmail.com>
2023-02-13 16:29:24 -0300

    refactor tailwind css, se instala sass de forma global dejo ejmeploss

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom"
 import SideBar from "../components/SideBar"
-
+import css from "../assets/styles/components/mainLayout.module.scss"
 const MainLayout = (props) => {
   return (
     <>
-      <main className="container mx-auto mt-5 p-5 md:mt-10  justify-center flex md:items-center gap-6">
+      <main className={css.container}>
         <SideBar />
         <div className="w-[32rem]">
           <Outlet />
